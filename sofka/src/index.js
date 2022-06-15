a28876346113d6634f46cfb4708f1837247184a8
Mariana-Palacios <marianitaaax29@gmail.com>
2022-06-15 10:23:36 -0500

    agregando background y planetas

--- sofka/src/index.js
+++ sofka/src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import SVGComponent from './SVGComponent';
+import space_background from "./img/background.svg";
+import './styles/index.css'
 // import './index.css';
 
 
-const Index = () =>{
-  return(
-    <div>    
-      <App nave="nave"/>
-      <SVGComponent/>
+function Index() {
+  return (
+    <div className='flex flex-f-d-c flex-a-c'>
+      <h1>Estación espacial <span>sofka</span></h1>
+      <img src={space_background} className="background" alt='background'/>
+      <App nave="nave" />
+      <SVGComponent />
     </div>
-  )
+  );
 }
 
 ReactDOM.render(<Index/>,document.getElementById('root'));
